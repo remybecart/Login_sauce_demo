537461335b0941fdf958fd9e25a14d3430b6e515
BECART Remy <becart.remy@gmail.com>
2022-12-27 17:14:15 +0100

    WIP: premier IT valide

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,11 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+/// <reference types="cypress"/>
+
+describe('Découverte dun petit scénario de test', () => {
+    it('displays saucedemo.com', () => {
+        cy.visit('https://www.bstackdemo.com/')
+        cy.get('#signin').click()
+        cy.get('#username > .css-yk16xz-control > .css-1hwfws3').type('demouser{enter}')
+        cy.get('.css-1wa3eu0-placeholder').type('testingisfun99{enter}')
+        cy.get('#login-btn').click()
+    });
 })
